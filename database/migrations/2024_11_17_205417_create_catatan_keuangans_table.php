a376bd786aa9b6d04ceddfd495e6d3da4daf50c0
beckham-kho <beckham44997@gmail.com>
2024-11-29 22:42:21 +0700

    fix: change again catatan keuangan field name

--- database/migrations/2024_11_17_205417_create_catatan_keuangans_table.php
+++ database/migrations/2024_11_17_205417_create_catatan_keuangans_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('catatan_keuangans', function (Blueprint $table) {
             $table->id();
             $table->string('kode_keuangan');
-            $table->date('tanggal');
+            $table->date('tanggal_keuangan');
             $table->integer('nominal');
             $table->string('kategori');
             $table->string('keterangan');
